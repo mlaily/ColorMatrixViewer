a83152c6ff2d3c73f0244727a80a6076673a9665
Melvyn <melvyn.laily@gmail.com>
2014-04-06 22:16:39 +0200

    refactoring... (moving files around) + added a title to the main form

--- ColorMatrixViewer/Form1.Designer.cs
+++ ColorMatrixViewer/Form1.Designer.cs
@@ -110,7 +110,7 @@
 			this.Controls.Add(this.menuStrip1);
 			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "ColorMatrix Viewer";
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
