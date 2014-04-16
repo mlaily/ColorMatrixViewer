df5808de52a8d54c36f963215c79468058c22de4
Melvyn <melvyn.laily@gmail.com>
2014-04-16 22:52:55 +0200

    set the image layout of the form back to its default value (does not matter anyway...)

--- ColorMatrixViewer/Form1.Designer.cs
+++ ColorMatrixViewer/Form1.Designer.cs
@@ -230,7 +230,6 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
 			this.ClientSize = new System.Drawing.Size(832, 421);
 			this.Controls.Add(this.imageDiff1);
 			this.Controls.Add(this.leftColumnPanel);
